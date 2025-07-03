605cdb166ce710de220c6fc5404ff80012778391
Anders2057 <manuelandrescross@gmail.com>
2025-07-03 18:12:59 +0200

    Add sprite Jump, extra Jump, physics material, hearder en scripts player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,24 @@ public class PlayerController : MonoBehaviour
     private Transform m_transformPlayer;
     private Animator m_animator;
 
-
+    [Header("Move and Jump Settings")]
     [SerializeField]private float speed;
+    private int direction = 1;
+    
+    [SerializeField] private bool isJumping;
+
+    [SerializeField] private float jumpForce;
+    [SerializeField] private int extraJumps;
+    [SerializeField] private int CounterExtraJumps;
     private int idSpeed;
 
-    private int direction = 1;
+    [Header("Ground Settings")]
+    [SerializeField] private Transform rFoot;
+    [SerializeField] private Transform lFoot;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float rayLegnth;
+    [SerializeField] private LayerMask groundLayer;
+    private int idIsGrounded;
 
     void Start()
     {
@@ -22,22 +35,34 @@ public class PlayerController : MonoBehaviour
         m_rigigbody2D = GetComponent<Rigidbody2D>();
         m_animator = GetComponent<Animator>();
         idSpeed = Animator.StringToHash("Speed");
+        idIsGrounded = Animator.StringToHash("IsGrounded");
+        lFoot = GameObject.Find("LFoot").GetComponent<Transform>();
+        rFoot = GameObject.Find("RFoot").GetComponent<Transform>();
+        CounterExtraJumps = extraJumps;
     }
     private void Update()
     {
         SetAnimatorValue();
     }
-
+    private void SetAnimatorValue()
+    {
+        m_animator.SetFloat(idSpeed, Mathf.Abs(m_rigigbody2D.linearVelocityX));
+        m_animator.SetBool(idIsGrounded, isGrounded);
+       
+    }
     void FixedUpdate()
     {
         Move();
+        jump();
+        CheckGound();
     }
+
     private void Move()
     {
         Flip();
         m_rigigbody2D.linearVelocity = new Vector2(speed * m_gatherInput.ValueX, m_rigigbody2D.linearVelocityY);
     }
-
+   
     private void Flip()
     {
         if(m_gatherInput.ValueX * direction < 0)
@@ -46,8 +71,35 @@ public class PlayerController : MonoBehaviour
             direction *= -1;
         }
     }
-    private void SetAnimatorValue()
+    private void jump()
     {
-        m_animator.SetFloat(idSpeed, Mathf.Abs(m_rigigbody2D.linearVelocityX));
+        if (m_gatherInput.IsJumping) 
+        {
+            if (isGrounded)
+            {
+                m_rigigbody2D.linearVelocity = new Vector2(speed * m_gatherInput.ValueX, jumpForce);
+            }
+            if(CounterExtraJumps > 0)
+            {
+                m_rigigbody2D.linearVelocity = new Vector2(speed * m_gatherInput.ValueX, jumpForce);
+                CounterExtraJumps--;
+            }
+        }
+        m_gatherInput.IsJumping = false;
+    }
+    private void CheckGound()
+    {
+        RaycastHit2D lFootRay = Physics2D.Raycast(lFoot.position, Vector2.down, rayLegnth, groundLayer);
+        RaycastHit2D rFootRay = Physics2D.Raycast(rFoot.position, Vector2.down, rayLegnth, groundLayer);
+
+        if (lFootRay || rFootRay)
+        {
+            isGrounded = true;
+            CounterExtraJumps = extraJumps;
+        }
+        else 
+        {
+            isGrounded = false;
+        }
     }
 }
